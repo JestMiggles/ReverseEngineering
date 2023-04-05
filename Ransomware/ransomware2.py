06ca3e2f0eba6da0ce9e7085af234607cd158caf
Miguel Fernandez <54916306+JestMiggles@users.noreply.github.com>
2023-04-05 15:25:41 -0600

    Wrote most of my report and added other programs

--- Ransomware/ransomware2.py
+++ Ransomware/ransomware2.py
@@ -1,4 +1,6 @@
 def decrypt():
+    
+    # Read in file to decrypt
     file = input("Please enter the name of the file you want to decrypt: ")
     newFile = file[:-7] # This is to remove the '.pay_up' at the end :P
     
@@ -6,16 +8,22 @@ def decrypt():
     fileInput = open(file, "rb")
     fileOutput = open(newFile, "w")
     
-    # Read input until all is read, and decrypt
+    xor = "1337"
+    i = 0
     byteRead = fileInput.read(1)
     while(byteRead):
-        byteRead = chr(int.from_bytes(byteRead, "little") ^ 0x34)
+        byteRead = chr(int.from_bytes(byteRead, "little" ^ ord(xor[i])))
         fileOutput.write(byteRead)
         byteRead = fileInput.read(1)
-    
+        if(i == 3):
+            i = 0
+        else:
+            i += 1
+        
     # Close file, then re-open it and print the contents
     #fileOutput.close()
     #fileOutput = open(newFile, "r")
     #print(fileOutput.read())
+        
     
 decrypt()
--- Ransomware/ransomware3.py
+++ Ransomware/ransomware3.py
@@ -1,21 +1,27 @@
 def decrypt():
+    
+    # Read in file to decrypt
     file = input("Please enter the name of the file you want to decrypt: ")
     newFile = file[:-7] # This is to remove the '.pay_up' at the end :P
     
     # Open the file to read from and file to write to
-    fileInput = open(file, "rb")
+    fileInput = open(file, "r")
     fileOutput = open(newFile, "w")
     
-    # Read input until all is read, and decrypt
-    byteRead = fileInput.read(1)
+    key = "R3V3R53"
+    byteRead = fileInput.read(7)
     while(byteRead):
-        byteRead = chr(int.from_bytes(byteRead, "little") ^ 0x34)
-        fileOutput.write(byteRead)
-        byteRead = fileInput.read(1)
-    
+        temp = ""
+        for i in range(0, len(byteRead)):
+            temp += chr(ord(byteRead[i]) ^ ord(key[i]))
+        fileOutput.write(temp)
+        byteRead = fileInput.read(7)
+        
     # Close file, then re-open it and print the contents
     #fileOutput.close()
     #fileOutput = open(newFile, "r")
     #print(fileOutput.read())
-    
+
+
+
 decrypt()
